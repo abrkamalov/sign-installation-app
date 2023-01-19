921827f1368025c0eeff58f5b75582eb76239154
Abror Kamalov <abrkamalov@gmail.com>
2023-01-19 06:22:11 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,14 +12,14 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBv0dOPQzi-KzNOaA-wmGwSgLBTMucku5M",
-  authDomain: "pickeco-39e96.firebaseapp.com",
-  databaseURL: "https://pickeco-39e96-default-rtdb.firebaseio.com",
-  projectId: "pickeco-39e96",
-  storageBucket: "pickeco-39e96.appspot.com",
-  messagingSenderId: "1091911572374",
-  appId: "1:1091911572374:web:937595dda311e3f553dbb7",
-  measurementId: "G-C0952F75MR"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
